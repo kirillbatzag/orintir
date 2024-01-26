0b9c1d4be35d7042ec3014f3cddbecb7b3a44b1c
kirill <kirillzagkr@gmail.com>
2024-01-26 16:08:20 +0300

    Начал делать отоброжения бд

--- app/src/main/java/com/example/orintir/adapter/menAdapter.kt
+++ app/src/main/java/com/example/orintir/adapter/menAdapter.kt
@@ -1,6 +1,38 @@
 package com.example.orintir.adapter
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.orintir.Database.ManModel
+import com.example.orintir.R
 
-class menAdapter {
+class MenAdapter(private val dataList: List<ManModel>) :
+    RecyclerView.Adapter<MenAdapter.MenViewHolder>() {
+
+    class MenViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val photoImageView: ImageView = itemView.findViewById(R.id.photo_men)
+        val nameTextView: TextView = itemView.findViewById(R.id.name_text)
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MenViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.men_item, parent, false)
+        return MenViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MenViewHolder, position: Int) {
+        val currentItem = dataList[position]
+        with(holder) {
+
+            nameTextView.text = currentItem.name
+
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return dataList.size
+    }
 }
